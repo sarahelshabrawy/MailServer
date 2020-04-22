ad4e7be26cec9a3c991f4fd417f96afe27efbdc0
RanaAyman <58454300+RanaAyman@users.noreply.github.com>
2020-04-22 23:01:38 +0200

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -115,6 +115,64 @@ public class Sort implements ISort {
         }
     }
 
+    //////////////////////////////////////////////////////////////////////
+    public int partition_date(String[] arr, int low, int high, int numberOfFilters) throws ParseException {
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd:HH:mm:ss");
+
+		String pivot = arr[high];
+		// Date date = df.parse(pivot);
+		int i = (low - 1);
+		int j;
+		for (j = low; j < high; j += numberOfFilters) {
+			if (df.parse(arr[j]).before(df.parse(pivot))) {
+				if (i == -1)
+					i++;
+				else
+					i += numberOfFilters;
+				String temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				if (i != j) {
+					for (int k = i + 1; k < i + numberOfFilters; k++) {
+						String temp2 = arr[k];
+						arr[k] = arr[k + numberOfFilters];
+						arr[k + numberOfFilters] = temp2;
+					}
+				}
+			}
+		}
+		String temp = arr[i + numberOfFilters];
+		arr[i + numberOfFilters] = arr[high];
+		arr[high] = temp;
+		int count = high;
+		for (int k = i + numberOfFilters + 1; k < i + (numberOfFilters * 2) && high != i + numberOfFilters; k++) {
+			count++;
+			String temp2 = arr[k];
+			arr[k] = arr[count];
+			arr[count] = temp2;
+		}
+
+		return i + numberOfFilters;
+	}
+
+	public void sort_date(String[] arr, int numberOfFilters) throws ParseException {
+		int len = ((arr.length / numberOfFilters) - 1) * numberOfFilters;
+		Stack sort = new Stack();
+		int pi = partition_date(arr, 0, len, numberOfFilters);
+		sort.push(new Point(pi + numberOfFilters, len));
+		sort.push(new Point(0, pi - numberOfFilters));
+		while (!sort.isEmpty()) {
+			Point temp = (Point) sort.pop();
+			if (temp.x < temp.y) {
+				pi = partition_date(arr, temp.x, temp.y, numberOfFilters);
+				sort.push(new Point(pi + numberOfFilters, temp.y));
+				sort.push(new Point(temp.x, pi - numberOfFilters));
+			}
+		}
+//		return arr;
+	}
+    ///////////////////////////////////////////////////////////////////////
+    
     public static void main(String[] args) {
         int [] haha = {8,5,2,3,6,9,7,4,1,0,9,85,3};
         Sort trial = new Sort();
